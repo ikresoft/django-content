59ee04ca79095018a9367dca227ee8a026c49c05
Enver Bisevac <enver@Envers-MacBook-Pro.local>
2015-09-07 10:32:09 +0200

    jinja support

---
+++ content/jinja2.py
@@ -0,0 +1,37 @@
+from categories.views import get_category_for_path
+from content.models import CategoryContent, Category
+
+
+def get_content_by_categories(categories, limit, random=False):
+    # convert to objects
+    try:
+        cats = [x.strip() for x in categories.split(',')]
+        categories = []
+        for cat in cats:
+            try:
+                sel_categories = get_category_for_path(cat, queryset=Category.objects.all())
+            except:
+                sel_categories = Category.objects.get(pk=int(cat))
+            categories.append(sel_categories)
+    except:
+        pass
+
+    if categories is None or categories == []:
+        return None
+    try:
+        query = CategoryContent.published.all()
+        if categories is not None and categories != []:
+            query = query.filter(categories__in=categories)
+
+        ordering = '-date_modified'
+        if random:
+            ordering = '?'
+        query = query.order_by(ordering)
+        if limit == -1:
+            return query.all()
+        else:
+            return query.all()[:limit]
+        if limit == 1:
+            return query[0]
+    except:
+        return None
